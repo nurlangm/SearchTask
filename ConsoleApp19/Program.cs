99d806d6e7c3099d45f0ac163aa380a2d64866cd
unknown <you@example.com>
2021-10-07 11:43:11 +0400

    Task

---
+++ ConsoleApp19/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApp19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string stringnew1 = Console.ReadLine();
+            string stringnew2 = Console.ReadLine();
+            Console.WriteLine(stringnew1.Search(stringnew2));
+        }
+    }
+    static class Extension
+    {   
+        public static bool Search(this string str, string letter)
+        {
+            int let=0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i].ToString().ToLower() == letter[0].ToString().ToLower())
+                {
+                    for (int n = i + 1, m = 1; n < letter.Length; n++, m++)
+                    {
+                        if (str[n].ToString().ToLower() == letter[m].ToString().ToLower())
+                        {
+                            let++;
+                        }
+                    }
+                }
+            }
+            if (let == letter.Length - 1)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
